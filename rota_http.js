2e636e0bffebfe97bebd0ee24fb4a975db817558
tallyto <rodrigues.tallyto@hotmail.com>
2023-06-23 23:48:31 -0300

    sugestão de correção

--- rota_http.js
+++ rota_http.js
@@ -1,34 +1,38 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function(request, response){
-    if(request.url == "/"){
-        fs.readFile(__dirname + '/artigos.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }else if(request.url == "/artigos"){
-        fs.readFile(__dirname + '/artigos.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }else if(request.url == "/contato"){
-        fs.readFile(__dirname + '/contato.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }else{
-        fs.readFile(__dirname + '/erro.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }
-    response.end();
+var server = http.createServer(function(req, res) {
+  if (req.url == "/") {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var readStream = fs.createReadStream(__dirname + '/artigos.html', 'utf8');
+    readStream.pipe(res);
+  } else if (req.url == "/artigos") {
+    fs.readFile(__dirname + '/artigos.html', function(err, html) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (err) {
+        res.write(err.toString()); // Converter o erro para string
+      }
+      res.end(html);
+    });
+  } else if (req.url == "/contato") {
+    fs.readFile(__dirname + '/contato.html', function(err, html) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (err) {
+        res.write(err.toString()); // Converter o erro para string
+      }
+      res.end(html);
+    });
+  } else {
+    fs.readFile(__dirname + '/erro.html', function(err, html) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (err) {
+        res.write(err.toString()); // Converter o erro para string
+      }
+      res.end(html);
+    });
+  }
 });
-server.listen(3000, function(){
-    console.log('Servidor rodando');
+
+server.listen(3000, function() {
+  console.log('Servidor rodando');
 });
