4903642ccc1961ea045b67df3e55903484b11a82
thefontes <thefontes@gmail.com>
2023-06-23 23:45:20 -0400

    Server ok

--- rota_http.js
+++ rota_http.js
@@ -1,34 +1,27 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function(request, response){
-    if(request.url == "/"){
-        fs.readFile(__dirname + '/artigos.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }else if(request.url == "/artigos"){
-        fs.readFile(__dirname + '/artigos.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }else if(request.url == "/contato"){
-        fs.readFile(__dirname + '/contato.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
-        });
-    }else{
-        fs.readFile(__dirname + '/erro.html', function(err, html){
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(err);
-            response.end(html);
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url == "/") {
+        var readStream = fs.createReadStream(__dirname + '/artigos.html', 'utf8');
+        readStream.pipe(res);
+    } else if (req.url == "/artigos") {
+        var readStream = fs.createReadStream(__dirname + '/artigos.html', 'utf8');
+        readStream.pipe(res);
+    } else if (req.url == "/contato") {
+        var readStream = fs.createReadStream(__dirname + '/contato.html', 'utf8');
+        readStream.pipe(res);
+    } else {
+        fs.readFile(__dirname + '/erro.html', function(err, html) {
+            if (err) {
+                res.write(err.toString()); // Converter o erro para string
+            }
+            res.end(html);
         });
     }
-    response.end();
 });
-server.listen(3000, function(){
+
+server.listen(3000, function() {
     console.log('Servidor rodando');
 });
